c20ce75f0fe5fdd71472ffe9c735a650a0a79e08
Anandhu-TS <92111880+Anandhu-TS@users.noreply.github.com>
2024-09-03 19:54:57 +0530

    Delete theusers directory

--- theusers/middleware.py
+++
@@ -1,8 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class DisableBackButtonMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        response['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
-        response['Pragma'] = 'no-cache'
-        response['Expires'] = '0'
-        return response
